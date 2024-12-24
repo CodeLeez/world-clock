f42006a7d341d2af8efc1adadf94646d3b93dd11
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2024-12-24 10:24:34 +0800

    Update real time every millisecond

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,23 @@
-let parisElement = document.querySelector("#paris");
-let parisDateElement = parisElement.querySelector(".date");
-let parisTimeElement = parisElement.querySelector(".time");
-let parisTime = moment();
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
 
-parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-parisTimeElement.innerHTML = parisTime.format("h:mm:ss[<small>]A[</small>]");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss:SSS [<small>]A[</small>]"
+  );
+
+  let beijingElement = document.querySelector("#beijing");
+  let beijingDateElement = beijingElement.querySelector(".date");
+  let beijingTimeElement = beijingElement.querySelector(".time");
+  let beijingTime = moment().tz("Asia/Beijing");
+
+  beijingDateElement.innerHTML = beijingTime.format("MMMM Do YYYY");
+  beijingTimeElement.innerHTML = beijingTime.format(
+    "h:mm:ss:SSS [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1);
